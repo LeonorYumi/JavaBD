002bfeca2f993c1cfed5feb34b5ea50de1613968
Leonor Yumi <leonoryumi@gmail.com>
2025-12-12 12:40:50 -0500

    Conexion MySQL

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,20 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    public static void main(String[] args) throws SQLException {
+
+        String url = "jdbc:mysql://localhost:3306/almacen";
+        String user = "root";
+        String password = "root";
+        Connection connection = DriverManager.getConnection(url, user, password);
+
+        if (connection != null) {
+            System.out.println("Conexion exitosa");
+            connection.close();
+        } else{
+            System.out.println("No se puede conectar con la base de datos");
         }
     }
 }
